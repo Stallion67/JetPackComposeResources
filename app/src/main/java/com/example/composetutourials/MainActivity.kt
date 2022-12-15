9657135ee73838ff379214192580ce62cf3edcd0
BEN OKELLO MWAKA <mwakaben@gmail.com>
2022-12-15 10:16:23 +0300

    basic setup and introduction

--- app/src/main/java/com/example/composetutourials/MainActivity.kt
+++ app/src/main/java/com/example/composetutourials/MainActivity.kt
@@ -3,41 +3,57 @@ package com.example.composetutourials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutourials.ui.theme.ComposeTutourialsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutourialsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(Message("Okello", "Learning","Compose"))
         }
     }
 }
 
+data class Message(val author:String,val body:String, val detail:String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(msg: Message) {
+  Row{
+      Image(painter = painterResource(id = R.drawable.unnamed),
+          contentDescription ="okello Profile",
+          modifier= Modifier
+              .size(60.dp) //set Image sire to 60dp
+              .clip(CircleShape) //clip image to be shaped as a circle
+          )
+
+      Spacer(modifier =Modifier.width(8.dp)) // adding Horizontal space between image ancolumn
+      Column {
+           Text(text = msg.author)
+          Text(text= msg.body)
+          
+          Spacer(modifier = Modifier.height(4.dp))
+          Text(text= msg.detail)
+      }
+  }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTutourialsTheme {
-        Greeting("Android")
+        Greeting(
+            msg = Message("hey", "compose" ,"by Okello")
+        )
     }
 }
